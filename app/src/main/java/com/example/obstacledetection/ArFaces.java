c7a1fc637c621971cac5de1bb4dfd6bdeacb829a
Orestiszar <zarasorestis@gmail.com>
2022-12-17 17:33:49 +0200

    Layout creation

---
+++ app/src/main/java/com/example/obstacledetection/ArFaces.java
@@ -0,0 +1,37 @@
+package com.example.obstacledetection;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ArFaces extends Fragment {
+
+    public ArFaces() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ar_faces, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.goto_home_button).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.backtohome_action));
+    }
+}
