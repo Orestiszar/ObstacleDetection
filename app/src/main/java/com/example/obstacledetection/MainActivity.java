ec605938ff5b595022c74e442d861fb2f4705d12
Orestiszar <zarasorestis@gmail.com>
2023-04-04 17:04:33 +0300

    Merged everythin into Main Activity. Text label for center distance

--- app/src/main/java/com/example/obstacledetection/MainActivity.java
+++ app/src/main/java/com/example/obstacledetection/MainActivity.java
@@ -1,21 +1,180 @@
 package com.example.obstacledetection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.NotYetAvailableException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.SceneView;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Collection;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements FragmentOnAttachListener, BaseArFragment.OnTapArPlaneListener, BaseArFragment.OnSessionConfigurationListener, ArFragment.OnViewCreatedListener{
     private ArFragment arFragment;
     private ViewRenderable viewRenderable;
     private String title = "Anchor";
+    private ArSceneView arSceneView;
+    private TextView testText;
+
+    private void buildModel() {
+        ViewRenderable.builder()
+                .setView(this, createArTile(title, getApplicationContext()))
+                .build()
+                .thenAccept(viewRenderable -> this.viewRenderable = viewRenderable)
+                .exceptionally(throwable -> {
+                    throwable.printStackTrace();
+                    Log.d("ERR", throwable.toString());
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+
+    public View createArTile(String title, Context context) {
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setBackgroundResource(R.drawable.rounded_border);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(8, 8, 8, 8);
+        TextView view = new TextView(context);
+        view.setText(title);
+        view.setTextSize(18);
+        view.setTextColor(Color.RED);
+        view.setPadding(8, 8, 8, 8);
+        linearLayout.addView(view);
+        return linearLayout;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+        buildModel();
+        this.testText = findViewById(R.id.testText);
     }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this);
+            arFragment.setOnViewCreatedListener(this);
+            arFragment.setOnTapArPlaneListener(this);
+        }
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+            config.setDepthMode(Config.DepthMode.AUTOMATIC);
+        }
+    }
+
+    @Override
+    public void onViewCreated(ArSceneView arSceneView) {
+        this.arSceneView = arSceneView;
+        arFragment.setOnViewCreatedListener(null);
+        this.arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL);
+        this.arSceneView.getScene().addOnUpdateListener(this::onSceneUpdate);
+    }
+
+    private void onSceneUpdate(FrameTime updatedTime) {
+//        Frame frame = arSceneView.getArFrame();
+//        Collection<Anchor> updatedAnchors = frame.getUpdatedAnchors();
+//        for (Anchor anchor : updatedAnchors) {
+//            // Handle updated anchors...
+//        }
+
+        Image depthImage = null;
+        Frame frame = this.arSceneView.getArFrame();
+        try {
+            depthImage = frame.acquireDepthImage16Bits();
+            // Use the depth image here.
+            if(depthImage == null){
+//                Toast.makeText(this, "depthImage Null", Toast.LENGTH_LONG).show();
+                this.testText.setText("Null");
+            }
+            else{
+                int debugdistance = getMillimetersDepth(depthImage, 80,45);
+                this.testText.setText(Integer.toString(debugdistance));
+
+            }
+        } catch (NotYetAvailableException e) {
+            // This means that depth data is not available yet.
+            // Depth data will not be available if there are no tracked
+            // feature points. This can happen when there is no motion, or when the
+            // camera loses its ability to track objects in the surrounding
+            // environment.
+        } finally {
+            if (depthImage != null) {
+                depthImage.close();
+            }
+        }
+    }
+
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (viewRenderable == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+        anchorNode.setRenderable(viewRenderable);
+    }
+
+    /** Obtain the depth in millimeters for depthImage at coordinates (x, y). */
+    public int getMillimetersDepth(Image depthImage, int x, int y) {
+        // The depth image has a single plane, which stores depth for each
+        // pixel as 16-bit unsigned integers.
+        Image.Plane plane = depthImage.getPlanes()[0];
+        int byteIndex = x * plane.getPixelStride() + y * plane.getRowStride();
+        ByteBuffer buffer = plane.getBuffer().order(ByteOrder.nativeOrder());
+        return buffer.getShort(byteIndex);
+    }
+
+
 }
